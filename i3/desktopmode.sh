de240298aeaef75d6df7b75897c8af96190f5eb5
Guillaume Fournier <giom@mailbox.org>
2022-11-08 00:17:19 +0100

    Remove i3 subdir

--- i3/desktopmode.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-xrandr --output eDP-1 --dpi 100 --mode 1920x1080 --pos 0x540 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --dpi 163  --mode 3840x2160 --pos 1920x0 --rotate normal --output HDMI-2 --off
-i3-msg restart > /dev/null
