e4deec707a3eb1d4bad7d5810f7a8d9bbabf5643
Guillaume Fournier <giom@mailbox.org>
2023-01-27 10:34:00 +0100

    Split aliases

---
+++ oh-my-bash/aliases/ls.aliases.sh
@@ -0,0 +1,5 @@
+# exa
+alias ls="exa"
+alias ll="exa -lbghF"
+alias la="exa -lbghFa"
+alias lt="exa --tree --level=2"
--- oh-my-bash/aliases/trash.aliases.sh
+++ oh-my-bash/aliases/trash.aliases.sh
@@ -1,9 +1,3 @@
-# exa
-alias ls="exa"
-alias ll="exa -lbghF"
-alias lt="exa --tree --level=2"
-alias la="exa -lbghFa"
-
 # trash-cli
 alias dust="du -hs * | sort -hr"
 alias rm='echo -e "Warning: \"rm\" will delete files permanently!\nUse \"trash\" instead to use a trashbin.\nUse \"\\\rm\" to bypass this warning."; false'
