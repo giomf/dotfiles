26c39bf719f2571525b510884ffb79197a77aeec
Guillaume Fournier <guillaume.fm@mailbox.org>
2023-01-29 14:25:26 +0100

    refactor theme

--- oh-my-bash/custom/themes/mytheme/mytheme.theme.sh
+++ oh-my-bash/custom/themes/mytheme/mytheme.theme.sh
@@ -1,11 +1,12 @@
 
-THEME_SHOW_USER_HOST="true"
-SCM_THEME_PROMPT_PREFIX="["
-SCM_THEME_PROMPT_SUFFIX="]"
+export THEME_SHOW_USER_HOST="true"
+export SCM_THEME_PROMPT_PREFIX="["
+export SCM_THEME_PROMPT_SUFFIX="]"
 
 function prompt_git_status {
-  local git_status=$(scm_prompt_info)
-  if [[  -n "$(echo ${git_status} | grep ${SCM_THEME_PROMPT_CLEAN})" ]]; then
+  local git_status
+  git_status=$(git_prompt_info)
+  if [[  ${SCM_STATE} == "${SCM_THEME_PROMPT_CLEAN}" ]]; then
     echo "${_omb_term_teal}${git_status}${_omb_term_normal}"
   else
     echo "${_omb_term_olive}${git_status}${_omb_term_normal}"
@@ -15,12 +16,21 @@ function prompt_git_status {
 
 _omb_theme_PROMPT_COMMAND() {
 
-    local clock="${_omb_prompt_white}$(clock_prompt)${_omb_term_normal}"
-    local user_host="${_omb_term_navy}$(user_host_prompt)$_omb_prompt_reset_color"
-    local path="${_omb_prompt_purple}\w${_omb_term_normal}"
+    local clock
+    clock="${_omb_prompt_white}$(clock_prompt)${_omb_term_normal}"
+    local user_host
+    user_host="${_omb_term_navy}$(user_host_prompt)$_omb_prompt_reset_color"
+    local path
+    path="${_omb_prompt_purple}\w${_omb_term_normal}"
 
-    PS1="${clock} ${user_host} $(prompt_git_status) ${path}\n$ "
-    PS2="> "
+    # Get SCM state
+    scm
+
+    if [[ ${SCM} == "${SCM_GIT}" ]]; then
+      PS1="${clock} ${user_host} $(prompt_git_status) ${path}\n$ "
+    else
+      PS1="${clock} ${user_host} ${path}\n$ "
+    fi
 }
 
 _omb_util_add_prompt_command _omb_theme_PROMPT_COMMAND
