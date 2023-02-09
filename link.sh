ee27806de6ab4d117e64a474d962dabda7cb11ba
Guillaume Fournier <>
2023-02-10 00:58:34 +0100

    fix link paths

--- link.sh
+++ link.sh
@@ -1,16 +1,18 @@
 #!/bin/bash -e
 
+readonly SCRIPT_PATH="$(dirname "$(readlink -f "$0")")"
+
 if [[ "$1" == "--help" || "$1" == "-h" || -z "$1" ]]; then
     script_name="$(basename "$0")"
     echo -e "${script_name} Usage:\n\tOptions:\n\t--bash"
     exit 0
 elif [[ "$1" == "--bash" ]]; then
     # Link bashrc
-    ln -s ./config/oh-my-bash/bashrc ~/.bashrc
+    ln -s "${SCRIPT_PATH}/oh-my-bash/bashrc" ~/.bashrc
 
     # Link custom folders
-    ln -s custom/aliases/* ~/.config/oh-my-bash/custom/aliases/
-    ln -s custom/themes/* ~/.config/oh-my-bash/custom/themes/
+    ln -s "${SCRIPT_PATH}/custom/aliases/*" ~/.config/oh-my-bash/custom/aliases/
+    ln -s "${SCRIPT_PATH}/custom/themes/*" ~/.config/oh-my-bash/custom/themes/
 else
     exit 1
 fi
