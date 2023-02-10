52bd0a7902d8316fb3b778c0ec2d691df5ac7bbf
Guillaume Fournier <>
2023-02-10 02:14:39 +0100

    Add install script Add linker script to oh my bash

---
+++ oh-my-bash/install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -e
+
+export OSH="$HOME/.config/oh-my-bash"
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
---
+++ oh-my-bash/link.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -e
+
+readonly SCRIPT_PATH="$(dirname "$(readlink -f "$0")")"
+
+function link_files_in_directory () {
+    local readonly source_path="$1"
+    local readonly destination_path="$2"
+
+    for file_path in $(find "${source_path}" -maxdepth 1 -type f); do
+        local readonly file_name=$(basename ${file_path})
+        ln -sf "${file_path}" "${destination_path}/${file_name}"
+    done
+}
+
+if [[ "$1" == "--help" || "$1" == "-h" ]]; then
+    script_name="$(basename "$0")"
+    echo -e "${script_name}: Links all needed files for oh-my-bash" 
+    exit 0
+else
+    # Link bashrc
+    ln -sf "${SCRIPT_PATH}/bashrc" "${HOME}/.bashrc"
+
+    # Link custom folders
+    link_files_in_directory "${SCRIPT_PATH}/custom/aliases" "${HOME}/.config/oh-my-bash/custom/aliases"
+    ln -sf "${SCRIPT_PATH}/custom/plugins/cd" "${HOME}/.config/oh-my-bash/custom/plugins/cd"
+    ln -sf "${SCRIPT_PATH}/custom/themes/mytheme" "${HOME}/.config/oh-my-bash/custom/themes"
+fi
+
+exit 0
+
